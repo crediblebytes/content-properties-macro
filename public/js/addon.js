c710144d70135d0f804810c8c1db4457ad5af121
Don Page <donwilliampage@live.com>
2017-10-21 09:53:23 -0400

    Minor changes to plugin

--- public/js/addon.js
+++ public/js/addon.js
@@ -1,1 +1,8 @@
 /* add-on script */
+$(function(){
+    AP.require(['confluence'], function(confluence) {
+        confluence.getContentProperty('job', function(property) {
+            console.log(property);
+        })
+    });
+});
